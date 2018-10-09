aa806e244982e461724541e77fb766c35181d443
arixking <arixking@gmail.com>
2018-10-09 00:00:53 -0400

    Add function to add list items, style components
    Note: we need to also eventually add the function to delete items.

--- js/invoice-calc.js
+++ js/invoice-calc.js
@@ -1,7 +1,12 @@
-calculate = function()
-{
-    var resources = document.getElementById('itemQuantity').value;
-    var minutes = document.getElementById('itemRate').value;
-    document.getElementById('itemTotal').value = parseInt(resources)*parseInt(minutes);
+function calculate() {
+  var resources = document.getElementById('itemQuantity').value;
+  var minutes = document.getElementById('itemRate').value;
+  document.getElementById('itemTotal').value = parseInt(resources)*parseInt(minutes);
+}
 
-   }
+
+function addItem() {
+    var elmnt = document.getElementById("lineItem");
+    var cln = elmnt.cloneNode(true);
+    document.getElementById("itemList").appendChild(cln);
+}
