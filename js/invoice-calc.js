10b6dd01011fbbefe6a63ea3e3a87f79b7058a28
arixking <arixking@gmail.com>
2018-10-11 19:53:32 -0400

    Add logo, sidebar content

--- js/invoice-calc.js
+++ js/invoice-calc.js
@@ -2,8 +2,8 @@
 var itemCount = 0
 
 function calculate() {
-  var resources = document.getElementById('itemQuantity').value;
-  var minutes = document.getElementById('itemRate').value;
+  var resources = document.getElementById("itemQuantity").value;
+  var minutes = document.getElementById("itemRate").value;
   document.getElementById('itemTotal').value = parseInt(resources)*parseInt(minutes);
 }
 
